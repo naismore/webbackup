f1ea865029aaebfa0f4c3ec9d01e238ee947bfc7
naismore <124817694+naismore@users.noreply.github.com>
2023-03-30 22:34:35 +0300

    1

--- pages/hello.go
+++ pages/hello.go
@@ -7,11 +7,20 @@ import (
 )
 
 func main() {
-	http.ListenAndServe(":3000", http.HandlerFunc(index))
+	const port = ":3000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/home", index)
+
+	log.Println("Start server " + port)
+	e := http.ListenAndServe(port, mux)
+	if e != nil {
+		log.Fatal(e)
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	ts, e := template.ParseFiles("pages/index.html")
+	ts, e := template.ParseFiles("index.html")
 	if e != nil {
 		http.Error(w, "Internal Server Errror", 500)
 		log.Println(e.Error())
@@ -26,7 +35,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		Subtitle: "Subtitle for my page",
 	}
 	e = ts.Execute(w, data)
-	if err != nil {
+	if e != nil {
 		http.Error(w, "Internal Server Error", 500)
 		log.Println(e.Error())
 		return
